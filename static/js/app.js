d1f949fbb52dd4c7f431b271801e9f13eda42fc2
sasha manohar <vsman.dev@gmail.com>
2021-03-09 21:49:46 -0500

    Added framework for data extraction

--- static/js/app.js
+++ static/js/app.js
@@ -12,3 +12,28 @@ var tbody = d3.select("tbody");
 function printHello() {
     console.log("Hello there!");
   }
+
+  // create a new function to guide our data import
+
+functio buildTable(data) {
+  // clear the table (which will contain our array) first, so that we have no duplicates from a prior search
+  tbody.html("");
+
+  // call an arrow function using the forEach method to create a for loop
+  data.forEach((dataRow) => {
+
+    // look for the <tbody> html tag and append a table row <tr>
+    let row = tbody.append("tr");
+
+    // Access one object's values from the currently iterated dataRow
+    // Object.values(dataRow) references one object and stores values in dataRow
+    // forEach((val) indicates one object per row
+    Object.values(dataRow).forEach((val) => {
+      // create a row for each value
+      let cell = row.append("td");
+      // add the text val into the cell
+      cell.text(val);
+    });
+
+  });
+}
