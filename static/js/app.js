c8052442f758e404a31f21965e225100bbfdcf7b
sasha manohar <vsman.dev@gmail.com>
2021-03-11 19:00:33 -0500

    Created framework for webpage

--- static/js/app.js
+++ static/js/app.js
@@ -46,9 +46,7 @@ function handleClick() {
     // Apply `filter` to the table data to only keep the
     // rows where the `datetime` value matches the filter value
     filteredData = filteredData.filter(row => row.datetime === date);
-
-    
-  };
+  }
   // Rebuild the t able using the re-assigned (filtered) data
   // If no date was entered, the data returned is the original filtered data
   buildTable(filteredData);
